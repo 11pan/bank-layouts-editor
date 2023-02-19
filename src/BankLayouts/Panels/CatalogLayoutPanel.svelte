6cea154b7787775327ce8a4085ad988d7146b203
Jere <Dynrothe@hotmail.com>
2023-02-19 20:40:42 +0200

    Fixed more issues

--- src/BankLayouts/Panels/CatalogLayoutPanel.svelte
+++ src/BankLayouts/Panels/CatalogLayoutPanel.svelte
@@ -4,10 +4,6 @@
 	import { ACTIVE_CATALOG_TAB, getCatalog, LAYOUT_CATALOG, TAG_CATALOG } from "../Utility/stores";
 	import { Tabs, Tab, Progress } from 'svelma';
 
-	let promise;
-
-	if (Object.entries($LAYOUT_CATALOG).length < 1 && Object.entries($TAG_CATALOG).length < 1) 
-		promise = getCatalog();
 
 	let active = 1;
 
@@ -16,19 +12,14 @@
 </script>
 
 <Tabs bind:active style="is-fullwidth">
-	{#await promise}
-		<Progress max="100"/>
-	{:then items} 
-		<Tab label='Tags' icon='tag'>
-			<TagCatalog/>
-		</Tab>
+	<Progress max="100"/>
+	<Tab label='Tags' icon='tag'>
+		<TagCatalog/>
+	</Tab>
 
-		<Tab label='Layouts' icon='th-large'>
-			<LayoutCatalog/>
-		</Tab>
-	{:catch error}
-		There was an error while trying to load the catalog: {JSON.stringify(error)}.
-	{/await}
+	<Tab label='Layouts' icon='th-large'>
+		<LayoutCatalog/>
+	</Tab>
 </Tabs>
 
 
