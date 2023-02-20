29ba1e9dcf491edb5bf7496017986ab7a4a45a1f
Jere <Dynrothe@hotmail.com>
2023-02-20 15:58:11 +0200

    Added pagination to browser and changed when welcome popup appears

--- src/BankLayouts/BankLayoutsApp.svelte
+++ src/BankLayouts/BankLayoutsApp.svelte
@@ -14,19 +14,21 @@
 	let itemsPromise = getItems();
 	let loadedFromUrl = false;
 
-	if ($WELCOME_POPUP === "true") {
-		Notification.create({message: 'Welcome to bank layout editor!<br><br>' + 
-		'This editor can be used to create or modify bank layouts<br>' + 
-		'for the Bank Tag Layouts Runelite plugin.<br><br>' + 
-		'Please ensure you have Bank Tag Layouts plugin installed<br>' + 
-		'Before you try exporting a layout into Runelite.<br><br>' + 
-		'If you encounter any issues or have suggestions,<br>' + 
-		'please create an issue ticket on <a href="https://github.com/11pan/bank-layouts-editor">Github</a>.<br><br>' + 
-		'<small>(This popup will not be shown again once you close it)</small>', position: "is-bottom-left", duration: 3600000 })
+	const ShowWelcomePopup = (val) => {
+		if ($WELCOME_POPUP === "true" && val) {
+			Notification.create({message: 'Welcome to bank layout editor!<br><br>' + 
+			'This editor can be used to create or modify bank layouts<br>' + 
+			'for the Bank Tag Layouts Runelite plugin.<br><br>' + 
+			'Please ensure you have Bank Tag Layouts plugin installed<br>' + 
+			'Before you try exporting a layout into Runelite.<br><br>' + 
+			'If you encounter any issues or have suggestions,<br>' + 
+			'please create an issue ticket on <a href="https://github.com/11pan/bank-layouts-editor">Github</a>.<br><br>' + 
+			'<small>(This popup will not be shown again once you close it)</small>', position: "is-bottom-left", duration: 3600000 })
 
-		document.getElementsByClassName("delete")[0].onclick = () => $WELCOME_POPUP = "false";
+			document.getElementsByClassName("delete")[0].onclick = () => $WELCOME_POPUP = "false";
+		}
+
 	}
-
 	const LoadLayoutFromQueryString = () => {
 
 		const urlParams = new URLSearchParams(window.location.search);
@@ -39,8 +41,8 @@
 	};
 
 	$: LoadLayoutFromQueryString($SLOTS.grid);
+	$: ShowWelcomePopup($SLOTS.grid)
 
-	
 </script>
 
 {#await itemsPromise}
--- src/BankLayouts/Components/TagCatalog.svelte
+++ src/BankLayouts/Components/TagCatalog.svelte
@@ -9,75 +9,121 @@
 
   $VISIBLE_TAG_CATALOG_ITEMS = $TAG_CATALOG;
 
+  const pageSize = 10;
+  let currentPage;
+  let pageIndex = 0;
+
+  let [pages, size] = [[...$VISIBLE_TAG_CATALOG_ITEMS], pageSize]
+  pages = [...Array(Math.ceil(pages.length / size))].map(_ => pages.splice(0,size))
+  currentPage = pages[0];
+
+  const UpdatePages = () => {
+    pages = [...$VISIBLE_TAG_CATALOG_ITEMS];
+    pages = [...Array(Math.ceil(pages.length / size))].map(_ => pages.splice(0,size))
+    ChangePage(0);
+  }
+
+  const ChangePage = (index) => {
+    currentPage = pages[index]
+    pageIndex = index;
+  }
+
+  $: UpdatePages($VISIBLE_TAG_CATALOG_ITEMS)
+
 </script>
 
-<div class="box">
-    {#each $VISIBLE_TAG_CATALOG_ITEMS as item}
-    <article class="media">
-        <div class="media-left">
-          <figure class="image is-64x64">
+<div>
+  <div class="box">
+    {#if pages.length > 0}
+      {#each $VISIBLE_TAG_CATALOG_ITEMS as item}
+      <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              {#if item}
+              <Icon id={GetIcon(item.tag, true)}/>
+              {/if}
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              {#if item}
+                <p>
+                  <Tooltip label={GetName(item.tag, true)} position="is-top" type="is-dark">
+                    <strong>{Titleize(item.name)}</strong>
+                  </Tooltip>
+                  <small style="color:darkgray;"> @{item.creator}</small>
+                  <small/>
+                  <br>
+                  {item.description}
+                </p>
+              {/if}
+            </div>
             {#if item}
-            <Icon id={GetIcon(item.tag, true)}/>
-            {/if}
-          </figure>
-        </div>
-        <div class="media-content">
-          <div class="content">
-            {#if item}
-              <p>
-                <Tooltip label={GetName(item.tag, true)} position="is-top" type="is-dark">
-                  <strong>{Titleize(item.name)}</strong>
-                </Tooltip>
-                <small style="color:darkgray;"> @{item.creator}</small>
-                <small/>
-                <br>
-                {item.description}
-              </p>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <button href={null} class="level-item hideBackground" aria-label="copy" on:click={LoadLayout(item.tag)}>
+                    <span class="icon is-small">
+                      <Tooltip label="Open tag" position="is-top" type="is-dark">
+                          <i class="fas fa-copy buttonColor" aria-hidden="true" />
+                      </Tooltip>
+                    </span>
+                  </button>
+                  <button href={null} class="level-item hideBackground" aria-label="save" on:click={SaveLayout(true, item.tag, true)}>
+                    <span class="icon is-small">
+                      <Tooltip label="Save tag" position="is-top" type="is-dark">
+                          <i class="fas fa-save buttonColor" aria-hidden="true" />
+                      </Tooltip>
+                    </span>
+                  </button>
+                  <button href={null} class="level-item hideBackground" aria-label="export" on:click={() => {
+                    navigator.clipboard.writeText(item.tag)
+                    Toast.create({ message: 'Layout copied to clipboard.', type: 'is-success', position: 'is-bottom-left' });
+                  }}>
+                    <span class="icon is-small">
+                      <Tooltip label="Export tag" position="is-top" type="is-dark">
+                          <i class="fas fa-file-export buttonColor" aria-hidden="true" />
+                      </Tooltip>
+                    </span>
+                  </button>
+                  <button href={null} class="level-item hideBackground" aria-label="share" on:click={GetShareUrl(item.tag)}>
+                    <span class="icon is-small">
+                      <Tooltip label="Share tag" position="is-top" type="is-dark">
+                          <i class="fas fa-share buttonColor" aria-hidden="true" />
+                      </Tooltip>
+                    </span>
+                  </button>
+                </div>
+              </nav>
             {/if}
           </div>
-          {#if item}
-            <nav class="level is-mobile">
-              <div class="level-left">
-                <button href={null} class="level-item hideBackground" aria-label="copy" on:click={LoadLayout(item.tag)}>
-                  <span class="icon is-small">
-                    <Tooltip label="Open tag" position="is-top" type="is-dark">
-                        <i class="fas fa-copy buttonColor" aria-hidden="true" />
-                    </Tooltip>
-                  </span>
-                </button>
-                <button href={null} class="level-item hideBackground" aria-label="save" on:click={SaveLayout(true, item.tag, true)}>
-                  <span class="icon is-small">
-                    <Tooltip label="Save tag" position="is-top" type="is-dark">
-                        <i class="fas fa-save buttonColor" aria-hidden="true" />
-                    </Tooltip>
-                  </span>
-                </button>
-                <button href={null} class="level-item hideBackground" aria-label="export" on:click={() => {
-                  navigator.clipboard.writeText(item.tag)
-                  Toast.create({ message: 'Layout copied to clipboard.', type: 'is-success', position: 'is-bottom-left' });
-                }}>
-                  <span class="icon is-small">
-                    <Tooltip label="Export tag" position="is-top" type="is-dark">
-                        <i class="fas fa-file-export buttonColor" aria-hidden="true" />
-                    </Tooltip>
-                  </span>
-                </button>
-                <button href={null} class="level-item hideBackground" aria-label="share" on:click={GetShareUrl(item.tag)}>
-                  <span class="icon is-small">
-                    <Tooltip label="Share tag" position="is-top" type="is-dark">
-                        <i class="fas fa-share buttonColor" aria-hidden="true" />
-                    </Tooltip>
-                  </span>
-                </button>
-              </div>
-            </nav>
-          {/if}
-        </div>
-      </article>
-    {/each}
-    {#if $VISIBLE_TAG_CATALOG_ITEMS.length < 1}
+        </article>
+      {/each}
+      {:else}
       <h1>Could not find any tags.</h1>
     {/if}
+  </div>
+
+  {#if pages.length > 1}
+    <div class="pagination">
+      <button on:click={() => {
+        if (pageIndex > 0) {
+          pageIndex--;
+          ChangePage(pageIndex)
+
+        }
+      }}>&laquo;</button>
+      {#each pages as item}
+      <button on:click={() => ChangePage(pages.indexOf(item))}>{pages.indexOf(item) + 1}</button>    
+    {/each}
+    <button on:click={() => { {
+        if (pageIndex < (pages.length - 1)) {
+          pageIndex++;
+          ChangePage(pageIndex)
+        }
+    }
+    }}>&raquo;</button>
+    </div>
+  {/if}
 </div>
 
 <style>
@@ -98,5 +144,31 @@
 
     .hideBackground:active {
       background-color: transparent;
+    }
+
+    
+    .pagination {
+      display: inline-block;
+    }
+
+    .pagination button {
+      color: #1abc9c;
+      background-color: #282f2f;
+      border-width: 2px;
+      border-color: #343c3d;
+      float: left;
+      padding: 8px 16px;
+      margin-left: 5px;
+      margin-right:5px;
+    }
+
+    .pagination button:active {
+      background-color: #2c3535;
+      color: #1aad90;
+    }
+
+    .pagination button:hover {
+      background-color: #2c3535;
+      color: #1aad90;
     }
 </style>
