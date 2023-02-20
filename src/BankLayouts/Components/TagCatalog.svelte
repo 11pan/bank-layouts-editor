2925bbc0e0aec0914d200ab8d11837ddae1c860c
Jere <Dynrothe@hotmail.com>
2023-02-20 16:21:30 +0200

    Made visual changes to browser

--- src/BankLayouts/Components/TagCatalog.svelte
+++ src/BankLayouts/Components/TagCatalog.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <div>
-  <div class="box">
+  <div class="box container">
     {#if pages.length > 0}
       {#each $VISIBLE_TAG_CATALOG_ITEMS as item}
       <article class="media">
@@ -127,48 +127,52 @@
 </div>
 
 <style>
-    .buttonColor {
-        color: #1abc9c;
-    }
+  .container {
+    min-height: 1031px;
+  }
 
-    .buttonColor:hover {
-        color: #12876f;
-    }
-
-    .hideBackground {
-      background-color: transparent;
-      border: none;
-      margin: 0px;
-      padding: 0px;
-    }
-
-    .hideBackground:active {
-      background-color: transparent;
-    }
-
-    
-    .pagination {
-      display: inline-block;
-    }
-
-    .pagination button {
+  .buttonColor {
       color: #1abc9c;
-      background-color: #282f2f;
-      border-width: 2px;
-      border-color: #343c3d;
-      float: left;
-      padding: 8px 16px;
-      margin-left: 5px;
-      margin-right:5px;
-    }
+  }
 
-    .pagination button:active {
-      background-color: #2c3535;
-      color: #1aad90;
-    }
+  .buttonColor:hover {
+      color: #12876f;
+  }
 
-    .pagination button:hover {
-      background-color: #2c3535;
-      color: #1aad90;
-    }
+  .hideBackground {
+    background-color: transparent;
+    border: none;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  .hideBackground:active {
+    background-color: transparent;
+  }
+
+  
+  .pagination {
+    display: inline-block;
+  }
+
+  .pagination button {
+    color: #1abc9c;
+    background-color: #282f2f;
+    border-width: 2px;
+    border-color: #343c3d;
+    float: left;
+    padding: 8px 16px;
+    margin-left: 5px;
+    margin-right:5px;
+  }
+
+  .pagination button:active {
+    background-color: #2c3535;
+    color: #1aad90;
+  }
+
+  .pagination button:hover {
+    background-color: #2c3535;
+    color: #1aad90;
+  }
 </style>
